50f46f7698e5a978538f89befb798cdfb053b905
tdudz <jdudney1@gmail.com>
2025-02-18 14:35:08 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,9 +4,9 @@ async function fetchNowPlaying() {
     const data = await response.json();
     if (data && data.album_art) {
     document.getElementById("album-art").src = data.album_art;
-} else {
+    } else {
     console.error("Album art not found");
-}
+    }
     document.getElementById("song-title").textContent = data.name;
     document.getElementById("artist-name").textContent = data.artist;
 }
